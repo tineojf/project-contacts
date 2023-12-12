0ec6d59e900d7cb219afc95324056146bb169615
tineojf <92743762+tineodev@users.noreply.github.com>
2023-12-12 18:28:26 -0500

    Add DatabaseConnector & demo

--- src/main/java/databaseConnector/DatabaseConnector.java
+++ src/main/java/databaseConnector/DatabaseConnector.java
@@ -1,8 +1,54 @@
-//package databaseConnector;
-//
-//public class DatabaseConnector {
-//    private static final DatabaseConnector instance = new DatabaseConnector();
-//    public
-//
-//
-//}
+package databaseConnector;
+
+import dotenv.ConfigEnv;
+
+import java.sql.*;
+
+public class DatabaseConnector {
+    private static final DatabaseConnector instance = new DatabaseConnector();
+    private Connection connection = null;
+
+    private DatabaseConnector() {
+        if (instance != null) {
+            throw new IllegalStateException("Ya existe una instancia de DatabaseConnector");
+        }
+    }
+
+    public static DatabaseConnector getInstance() {
+        return instance;
+    }
+
+    public Connection getConnection() {
+        ConfigEnv configEnv = ConfigEnv.getInstance();
+
+        try {
+            String url = "jdbc:mysql://"
+                    + configEnv.get("DATABASE_HOST")
+                    + ":"
+                    + configEnv.get("DATABASE_PORT")
+                    + "/"
+                    + configEnv.get("DATABASE_NAME");
+            String user = configEnv.get("DATABASE_USER");
+            String password = configEnv.get("DATABASE_PASSWORD");
+
+            connection = DriverManager.getConnection(url, user, password);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQLException: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+                System.out.println("Conexión cerrada");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQLException: " + e.getMessage());
+        }
+    }
+}
--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,18 +1,42 @@
 package main;
 
+import databaseConnector.DatabaseConnector;
 import dotenv.ConfigEnv;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
     public static void main(String[] args) {
         try {
-            System.out.println("Hello World!");
-            ConfigEnv configEnv = ConfigEnv.getInstance();
+            Class.forName("com.mysql.cj.jdbc.Driver");
 
-            System.out.println(configEnv.get("DATABASE_NAMEs"));
+            // Demo de ConfigEnv
+//            System.out.println("Hello World!");
+//            ConfigEnv configEnv = ConfigEnv.getInstance();
+//            System.out.println(configEnv.get("DATABASE_NAME"));
+
+            // Demo de DatabaseConnector
+            DatabaseConnector databaseConnector = DatabaseConnector.getInstance();
+            Connection connection = databaseConnector.getConnection();
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM amigos");
+            while (rs.next()) {
+                int x = rs.getInt("id");
+                String s = rs.getString("nombre");
+                System.out.println("ID: " + x + " Name: " + s);
+            }
+            databaseConnector.closeConnection();
 
             System.out.println("Hola");
         } catch (RuntimeException e) {
             System.err.println("Error en la aplicación: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
