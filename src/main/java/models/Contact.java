6bd9cb438c66566c3940091fc04486ee3fbe98d1
tineojf <92743762+tineodev@users.noreply.github.com>
2023-12-13 09:55:20 -0500

    Add model contact

---
+++ src/main/java/models/Contact.java
@@ -0,0 +1,65 @@
+package models;
+
+public class Contact {
+    private int id;
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    // Constructor
+    public Contact(int id, String name, String lastName, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // To String
+    @Override
+    public String toString() {
+        return this.name + " " + this.lastName + " - " + this.phone;
+    }
+}
