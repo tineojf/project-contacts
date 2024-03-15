f932ac84c261bd751348756594f25eaa92835524
tineojf <92743762+tineodev@users.noreply.github.com>
2024-03-15 16:30:09 -0500

    Update main class

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -13,10 +13,9 @@ public class App {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             // Demo de ConfigEnv
-            System.out.println("Hello World!");
-            ConfigEnv configEnv = ConfigEnv.getInstance();
-            System.out.println(configEnv.get("DATABASE_NAME"));
-            String databaseTableUsers = configEnv.get("DATABASE_TABLE_USERS");
+            //System.out.println("Hello World!");
+            //ConfigEnv configEnv = ConfigEnv.getInstance();
+            //System.out.println(configEnv.get("DATABASE_NAME"));
 
             // Demo de DatabaseConnector
             DatabaseConnector databaseConnector = DatabaseConnector.getInstance();
@@ -25,12 +24,12 @@ public class App {
             // Demo de Statement - POST
             ContactModel contact = new ContactModel(1, "Juan", "Perez", "999888777", "email@gmail.com");
             ContactDao contactDao = new ContactDao();
-//            contactDao.postContact(contact);
+            //contactDao.postContact(contact);
 
             // Demo de DatabaseConnector closeConnection
             databaseConnector.closeConnection();
 
-            System.out.println("Hola");
+            System.out.println("Programa finalizado");
         } catch (RuntimeException | ClassNotFoundException e) {
             System.err.println("Error en la aplicación: " + e.getMessage());
         }
