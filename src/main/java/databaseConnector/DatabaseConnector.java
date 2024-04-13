5fcc53fc84090558d98c5659dd1e6cdc3e403fb8
tineojf <92743762+tineodev@users.noreply.github.com>
2024-04-13 09:29:47 -0500

    Fix DatabaseConnector file

--- src/main/java/databaseConnector/DatabaseConnector.java
+++ src/main/java/databaseConnector/DatabaseConnector.java
@@ -18,42 +18,54 @@ public class DatabaseConnector {
         return instance;
     }
 
-    public Connection getConnection() {
+    private String[] loadDataDB() {
         ConfigEnv configEnv = ConfigEnv.getInstance();
+        String[] keys = new String[3];
 
-        try {
-            String databaseHost = configEnv.get("DATABASE_HOST");
-            String databasePort = configEnv.get("DATABASE_PORT");
-            String databaseName = configEnv.get("DATABASE_NAME");
-            String databaseUser = configEnv.get("DATABASE_USER");
-            String databasePassword = configEnv.get("DATABASE_PASSWORD");
+        String databaseHost = configEnv.get("DATABASE_HOST");
+        String databasePort = configEnv.get("DATABASE_PORT");
+        String databaseName = configEnv.get("DATABASE_NAME");
+        String databaseUser = configEnv.get("DATABASE_USER");
+        String databasePassword = configEnv.get("DATABASE_PASSWORD");
 
-            if (databaseHost == null || databasePort == null || databaseName == null || databaseUser == null || databasePassword == null) {
-                throw new IllegalStateException("Error en la configuración de la base de datos");
-            }
-
-            String databaseURL = "jdbc:mysql://" + databaseHost + ":" + databasePort + "/" + databaseName;
-
-            connection = DriverManager.getConnection(databaseURL, databaseUser, databasePassword);
-
-        } catch (SQLException e) {
-            //e.printStackTrace();
-            System.out.println("SQLException: " + e.getMessage());
-        } catch (IllegalStateException e) {
-            //e.printStackTrace();
-            System.out.println("IllegalStateException: " + e.getMessage());
-        } catch (Exception e) {
-            //e.printStackTrace();
-            System.out.println("Exception: " + e.getMessage());
+        if (databaseHost == null || databasePort == null || databaseName == null || databaseUser == null || databasePassword == null) {
+            throw new IllegalStateException("Error en la configuración de la base de datos");
+        } else {
+            keys[0] = "jdbc:mysql://" + databaseHost + ":" + databasePort + "/" + databaseName;
+            keys[1] = databaseUser;
+            keys[2] = databasePassword;
+            return keys;
         }
-        return connection;
+    }
+
+    private void connectToDB() throws SQLException, ClassNotFoundException {
+        String[] databaseURL = this.loadDataDB();
+
+        connection = DriverManager.getConnection(databaseURL[0], databaseURL[1], databaseURL[2]);
+
+        System.out.println("Successful connection");
+        String databaseName = connection.getMetaData().getDatabaseProductName();
+        String databaseVersion = connection.getMetaData().getDatabaseProductVersion();
+        System.out.println(databaseName + " " + databaseVersion);
+    }
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                this.connectToDB();
+            }
+            return connection;
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Error DB: " + e.getMessage());
+            return null;
+        }
     }
 
     public void closeConnection() {
         try {
             if (connection != null) {
                 connection.close();
-                System.out.println("Conexión cerrada");
+                System.out.println("Connection closed");
             }
         } catch (SQLException e) {
             System.out.println("SQLException: " + e.getMessage());
