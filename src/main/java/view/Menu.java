4b26827f6c3086412750db0649dc98ccba18e8ed
tineojf <92743762+tineodev@users.noreply.github.com>
2024-04-18 15:51:15 -0500

    Add method readOption & executeAction

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -1,6 +1,10 @@
 package view;
 
+import java.util.Scanner;
+
 public class Menu {
+    private final Scanner scanner = new Scanner(System.in);
+
     public void showMenu() {
         System.out.println("Bienvenido a la agenda de contactos");
         System.out.println("1. Listar contactos");
@@ -8,5 +12,53 @@ public class Menu {
         System.out.println("3. Actualizar contacto");
         System.out.println("4. Eliminar contacto");
         System.out.println("5. Salir");
+
+        int option = readOption(5);
+        System.out.println("Opción seleccionada: " + option);
+    }
+
+    public int readOption(int limit) {
+        int option;
+        boolean incorrectOption = true;
+
+        do {
+            System.out.print("Seleccione una opción: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            if (option >= 1 && option <= limit) {
+                incorrectOption = false;
+            } else {
+                System.out.println("Opción incorrecta, intente de nuevo");
+            }
+
+        } while (incorrectOption);
+
+        scanner.close();
+        return option;
+    }
+
+
+    public void executeAction(int option) {
+        switch (option) {
+            case 1:
+                System.out.println("Listar contactos");
+                break;
+            case 2:
+                System.out.println("Crear contacto");
+                break;
+            case 3:
+                System.out.println("Actualizar contacto");
+                break;
+            case 4:
+                System.out.println("Eliminar contacto");
+                break;
+            case 5:
+                System.out.println("Salir");
+                break;
+            default:
+                System.out.println("Opción no válida");
+                break;
+        }
     }
 }
