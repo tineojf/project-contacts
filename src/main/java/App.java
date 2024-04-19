bf3cbdee8a5711eb43ffdb6c1342f9b526c7964c
tineojf <92743762+tineodev@users.noreply.github.com>
2024-04-19 11:01:23 -0500

    Version 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,7 @@ import view.Menu;
 public class App {
     public static void main(String[] args) {
         try {
+            System.out.println("Project: Agenda de contactos");
             Menu menu = new Menu();
             menu.showMenu();
 
